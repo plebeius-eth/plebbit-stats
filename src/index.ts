839fd0352adda37abbed18ee191589c80ba9b07a
plebeius.eth <mottoes_recap0u@icloud.com>
2024-05-13 17:37:13 +0200

    add https

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import helmet from 'helmet';
+import https from 'https';
+import fs from 'fs';
 import rateLimit from 'express-rate-limit';
 import { getStats } from './controllers/stats-controller.js';
 import { updateStatsCache } from './services/plebbit.service.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(helmet());
 
@@ -23,12 +24,20 @@ app.get('/api/stats', getStats);
 async function startServer() {
   try {
     await updateStatsCache();
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
+
+    // add paths to ssl key and certificate to .env file
+    const privateKey = fs.readFileSync(process.env.SSL_PRIVATE_KEY_PATH, 'utf8');
+    const certificate = fs.readFileSync(process.env.SSL_CERTIFICATE_PATH, 'utf8');
+
+    const credentials = { key: privateKey, cert: certificate };
+
+    https.createServer(credentials, app).listen(port, () => {
+      console.log(`HTTPS Server running on https://localhost:${port}`);
     });
   } catch (error) {
     console.error('Failed to start server:', error);
   }
 }
 
+const port = process.env.PORT || 3000;
 startServer();
